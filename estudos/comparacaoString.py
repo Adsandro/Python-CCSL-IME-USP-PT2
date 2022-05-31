48b2307039b7c76bbafeb39bc65f4f57f8e53bf0
Adsandro Carvalho <adsandroxerd@gmail.com>
2022-05-31 11:31:03 -0300

    Utilizando metodos de comparação com string

---
+++ estudos/comparacaoString.py
@@ -0,0 +1,14 @@
+x = "teoria"
+y = "pratica"
+z = "teoria"
+
+print(x == z)
+print(x is z)
+print(x == y)
+
+print(x > y)
+#Esse tipo de comparação utiliza os códigos unicode para comparar ordem lexicografica
+print(ord("t"))
+print(ord("p"))
+
+
