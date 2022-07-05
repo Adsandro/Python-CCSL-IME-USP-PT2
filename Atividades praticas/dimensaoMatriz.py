d2b1306eba963ac9a2e446f0cd373f9874279210
Adsandro Carvalho <adsandroxerd@gmail.com>
2022-07-04 21:11:35 -0300

    Dimensão matriz

--- Atividades praticas/dimensaoMatriz.py
+++ Atividades praticas/dimensaoMatriz.py
@@ -1,7 +1,8 @@
-matriz = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
-linhas = 0
-colunas = 0
-def dimensoes(matriz):
-    for l in range(0, 3):
-        for c in range(0, 3)
-    return dimensoes
+class matriz:
+    def dimensoes(self, matriz):
+        linhas = len(matriz)
+        coluna = (len[0](matriz))
+        
+        print('A matriz possui {} linhas e {} colunas' .format(linhas, coluna))
+
+
